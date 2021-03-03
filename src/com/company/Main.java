16e33892ad043823b6981627de04f1a9900bef75
Thioro <dthioro@yahoo.fr>
2021-03-03 16:58:55 +0100

    (rajout de printf et mise en page)

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,50 +6,59 @@ public class Main {
 
     public static void main(String[] args) {
 
-            // On déclare un scanner pour lire les entrées de la console
-            Scanner sc = new Scanner(System.in);
+        // On déclare un scanner pour lire les entrées de la console
+        Scanner sc = new Scanner(System.in);
 
-// On demande le nombre de plat
-        System.out.println("Entrez Le nombre de plat : ");
-        int nbPlat = sc.nextInt();
-        sc.nextLine();
 
-    // On déclare et initialise le nombre total de comande et la variable d'aggrégation
-        float totalCommande=0;
-        int i=1;
-        String listCommande="";
+        // On déclare et initialise le nombre total de comande et la variable d'aggrégation
+        float totalCommande = 0;
+
+        String listCommande = "";
+        String description = "bonjour";
 
         //on fait la boucle
-        while(i<=nbPlat) {
 
+        while (!description.isEmpty()) {
             // On affiche un message pour demander la description de la commande
             System.out.println("Entrez la description de la commande : ");
 
-            // On récupère le prénom de la console
-            String description = sc.nextLine();
+            // On récupère le la description de la console
+            description = sc.nextLine();
 
-            // On demande la quantité
-            System.out.println("Entrez La quantité : ");
-            int quantite = sc.nextInt();
-            sc.nextLine();
+            //si la description est vide on arrête là, par compte s'il ya une entrée on pose les questions suivantes
+            if (!description.isEmpty()) {
+                // On demande la quantité
+                System.out.println("Entrez La quantité : ");
+                int quantite = sc.nextInt();
+                sc.nextLine();
 
-            // On demande le prix
-            System.out.println("Entrez Le prix : ");
-            float prix = sc.nextFloat();
-            sc.nextLine();
-              // on recupere le total de la commande aggrégé avec l'ancien total s'il existe
-            totalCommande =totalCommande + (quantite * prix);
-            // on concaténe l'ensemble des descriptions dans une récap (listCommande) avec la quantité, le prix unitaire et le total par commande en affichant la devise
-            listCommande= +quantite+"* "+description +  "  ( "+prix+ "€)    "+ totalCommande+"€\n"+ listCommande;
-            i++;
+                // On demande le prix
+                System.out.println("Entrez Le prix : ");
+                float prix = sc.nextFloat();
+                sc.nextLine();
+                float commandeU = quantite * prix;
+                // on recupere le total de la commande aggrégé avec l'ancien total s'il existe
+                totalCommande = totalCommande + commandeU;
+                // on concaténe l'ensemble des descriptions dans une récap (listCommande) avec la quantité, le prix unitaire et le total par commande en affichant la devise
+                //listCommande=  listCommande+"\n"+quantite+"* "+description +  "  ( "+prix+ "€)    "+ commandeU +"€";
+                listCommande = String.format("%s%n %d*%-17s  (%.1f€)  %.1f€", listCommande, quantite, description, prix, commandeU);
+
+                //prochain Etape mettre en forme dans un tableau et mettre la liste
+            }
+
+
         }
 
         // imprimer le ticket à la fin dans une mise en forme et la recap des commandes
-        System.out.println("----------------Facture-------------\n" +listCommande+ "\n-------Total de : " + totalCommande+"€---------");
+        /
+        System.out.println("----------------Facture-------------\nCommande             | Prix | total\n------------------------------------");
+
+        System.out.printf(" %s%n%n", listCommande);
+
+        System.out.printf("---------------Total de: %.1f€--------", totalCommande);
 
 
-
-            sc.close();
-        }
+        sc.close();
+    }
 
 }
